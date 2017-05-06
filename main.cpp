a5a3753f1ca73bd85ab76e059b312f6365bcae44
Jameson Thatcher <jameson@bluescreenofdoom.com>
2017-05-06 01:55:10 +0100

    Moved classes into Tee namespace

--- main.cpp
+++ main.cpp
@@ -2,29 +2,34 @@
 #include <iostream>
 
 Tee_Test(test_one_plus_one_is_two_succeeds) {
+	std::cout << GetName() << " running" << std::endl;
 	assert(1 + 1 == 2);
 }
 
 Tee_Test(test_one_plus_one_is_three_fails) {
+	std::cout << GetName() << " running" << std::endl;
 	assert(1 + 1 == 3);
 }
 
 Tee_Test(failing_one_assert_when_testing_two_produces_one_error) {
+	std::cout << GetName() << " running" << std::endl;
 	assert(2 + 2 == 4);
 	assert(2 + 2 == 5);
 };
 
 Tee_Test(failed_test_fails) {
+	std::cout << GetName() << " running" << std::endl;
 	assert(false);
 }
 
 int main() {
-	TestsHolder::Instance().RunAllTests();
-	auto fails = TestsHolder::Instance().GetFails();
+	int successes;
+	std::vector<std::string> fails;
+	std::tie(successes, fails) = Tee::RunAllTests();
 	for(auto f : fails)
 		std::cout << f;
 
-	std::cout << TestsHolder::Instance().GetSuccesses() << " successes, "
+	std::cout << successes << " successes, "
 		<< fails.size() << " failures (3 expected).";
 	return fails.size() == 3 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
--- tee.hpp
+++ tee.hpp
@@ -1,55 +1,67 @@
 #include <vector>
 #include <string>
+#include <tuple>
 
-class TestInterface {
-	public:
-		virtual void Run() = 0;
-		virtual std::string GetName() const = 0;
-};
+class Tee {
+public:
+	class TestInterface {
+		public:
+			virtual void Run() = 0;
+			virtual std::string GetName() const = 0;
+	};
 
-class TestsHolder {
-	public:
-		static TestsHolder& Instance() {
-			static TestsHolder o{};
-			return o;
-		}
+	class TestsHolder {
+		public:
+			static TestsHolder& Instance() {
+				static TestsHolder o{};
+				return o;
+			}
 
-		void AddTest(TestInterface& test) {
-			tests.push_back(&test);
-		}
+			void AddTest(TestInterface& test) {
+				tests.push_back(&test);
+			}
 
-		void AddFail(std::string fail) {
-			fails.emplace_back(fail);
-		}
+			void AddFail(std::string fail) {
+				fails.emplace_back(fail);
+			}
 
-		void AddSuccess() {
-			++successCount;
-		}
+			void AddSuccess() {
+				++successCount;
+			}
 
-		void RunAllTests() {
-			for(auto t : tests)
-				t->Run();
-		}
+			void RunAllTests() {
+				for(auto t : tests)
+					t->Run();
+			}
 
-		std::vector<std::string> const& GetFails() const {
-			return fails;
-		}
+			std::vector<std::string> const& GetFails() const {
+				return fails;
+			}
 
-		int GetSuccesses() const {
-			return successCount;
-		}
+			int GetSuccesses() const {
+				return successCount;
+			}
 
-	private:
-		std::vector<TestInterface*> tests {};
-		std::vector<std::string> fails {};
-		int successCount{};
+		private:
+			std::vector<TestInterface*> tests {};
+			std::vector<std::string> fails {};
+			int successCount{};
+	};
+
+	static std::pair<int, std::vector<std::string>> RunAllTests() {
+		TestsHolder::Instance().RunAllTests();
+		return std::make_pair(
+			TestsHolder::Instance().GetSuccesses(),
+			TestsHolder::Instance().GetFails()
+		);
+	}
 };
 
 #define Tee_Test(name) \
-	class Test_##name : public TestInterface { \
+	class Test_##name : public Tee::TestInterface { \
 		public: \
 			Test_##name() { \
-				TestsHolder::Instance().AddTest(*this); \
+				Tee::TestsHolder::Instance().AddTest(*this); \
 			} \
 			std::string GetName() const override { return #name; } \
 			void Run() override; \
@@ -63,8 +75,8 @@ class TestsHolder {
 
 #define assert(condition) \
 	if(condition != true) { \
-		TestsHolder::Instance().AddFail( \
+		Tee::TestsHolder::Instance().AddFail( \
 				__FILE__ "(" S__LINE__ "): " \
 				+ GetName() + " - " + #condition + "\n"); \
-	} else TestsHolder::Instance().AddSuccess();
+	} else Tee::TestsHolder::Instance().AddSuccess();
 
